885e3a07877b92662b0fcd99c1da25ed6f51f6f6
odiaz95012 <odiaz95012@gmail.com>
2024-02-19 22:24:39 -0800

    Fixing download again.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function downloadResume() {
-  const baseUrl = window.location.origin;
   const link = document.createElement("a");
-  link.href = baseUrl + "/oscar_diaz_vega_resume.pdf";
+  link.href = "oscar_diaz_vega_resume.pdf";
   link.download = "oscar_diaz_vega_resume.pdf";
   link.click();
 }
