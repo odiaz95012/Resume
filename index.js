40c48053be55ba4c55a33d3425bb404e0b32d47c
odiaz95012 <odiaz95012@gmail.com>
2024-02-19 22:29:08 -0800

    Final download fix.

--- index.js
+++
@@ -1,8 +0,0 @@
-function downloadResume() {
-  const link = document.createElement("a");
-  link.href = "oscar_diaz_vega_resume.pdf";
-  link.download = "oscar_diaz_vega_resume.pdf";
-  link.click();
-}
-
-
